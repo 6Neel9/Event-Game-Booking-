36c87470fefa73dfe380d73aa481023c0a978035
6Neel9 <88641136+6Neel9@users.noreply.github.com>
2023-04-06 16:04:49 +0530

    Update Checkout.jsx

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -36,6 +36,7 @@ const Checkout = () => {
   const [first, setFirst] = useState(false);
   const [second, setSecond] = useState(false);
   const [locate, setLocate] = useState("");
+  const [duration, setDuration] = useState("Set date and press enquiry button to see update");
 
   // Prev day logic
 
@@ -96,94 +97,119 @@ const Checkout = () => {
     // get minutes of event
     var diff = Math.abs(new Date(date.EndDate) - new Date(date.StartDate));
     var minutes = Math.floor(diff / 1000 / 60);
-
-    console.log(convertMinutes(minutes));
-    console.log(locate);
+    if (date.StartDate < date.EndDate) {
+      setDuration(convertMinutes(minutes));
+    } else {
+      setDuration("Please correct your date");
+    }
   };
 
   return (
     <div>
-     <div>
-     {fill ? (
-        <h2 style={{ color: "red" }}>Please fill all the fields</h2>
-      ) : (
-        <></>
-      )}
       <div>
-        <label htmlFor="evDate">Event Start date and time&nbsp;&nbsp;</label>
-        <input
-          type="datetime-local"
-          name="StartDate"
-          onChange={(e) => handleChange(e)}
-          value={date.StartDate}
-          id="evDate"
-        />
+        {fill ? (
+          <h2 style={{ color: "red" }}>Please fill all the fields</h2>
+        ) : (
+          <></>
+        )}
+        <div>
+          <label htmlFor="evDate">Event Start date and time&nbsp;&nbsp;</label>
+          <input
+            type="datetime-local"
+            name="StartDate"
+            onChange={(e) => handleChange(e)}
+            value={date.StartDate}
+            id="evDate"
+          />
+        </div>
+        {first ? (
+          <h3 style={{ color: "red" }}>
+            Event end date and time should not be before setup date and time
+          </h3>
+        ) : (
+          <></>
+        )}
+        <div>
+          <label htmlFor="evDateEnd">Event End date and time&nbsp;&nbsp;</label>
+          <input
+            type="datetime-local"
+            name="EndDate"
+            onChange={(e) => handleChange(e)}
+            value={date.EndDate}
+            id="evDateEnd"
+          />
+        </div>
+        {second ? (
+          <h3 style={{ color: "red" }}>
+            Setup date and time should not be after event date
+          </h3>
+        ) : (
+          <></>
+        )}
+        {setUpValidate ? (
+          <h3 style={{ color: "red" }}>
+            Setup date can not be more than 1 day before event
+          </h3>
+        ) : (
+          <></>
+        )}
+        <div>
+          <label htmlFor="setDate">Set Up date and time&nbsp;&nbsp;</label>
+          <input
+            type="datetime-local"
+            name="SetupDate"
+            onChange={(e) => handleChange(e)}
+            value={date.SetupDate}
+            id="setDate"
+          />
+        </div>
+        {locate == "" && <h3 style={{ color: "red" }}>Select any</h3>}
+        <div style={{ marginTop: "2%" }}>
+          <label htmlFor="location">Location&nbsp;&nbsp;</label>
+          <select
+            id="location"
+            value={locate}
+            onChange={(e) => handleChanges(e)}
+          >
+            {details.map((option) => (
+              <option key={option.Id} value={option.Id}>
+                {option.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button className="submita" onClick={submit}>
+          Enquiry
+        </button>
       </div>
-      {first ? (
-        <h3 style={{ color: "red" }}>
-          Event end date and time should not be before setup date and time
-        </h3>
-      ) : (
-        <></>
-      )}
-      <div>
-        <label htmlFor="evDateEnd">Event End date and time&nbsp;&nbsp;</label>
-        <input
-          type="datetime-local"
-          name="EndDate"
-          onChange={(e) => handleChange(e)}
-          value={date.EndDate}
-          id="evDateEnd"
-        />
-      </div>
-      {second ? (
-        <h3 style={{ color: "red" }}>
-          Setup date and time should not be after event date
-        </h3>
-      ) : (
-        <></>
-      )}
-      {setUpValidate ? (
-        <h3 style={{ color: "red" }}>
-          Setup date can not be more than 1 day before event
-        </h3>
-      ) : (
-        <></>
-      )}
-      <div>
-        <label htmlFor="setDate">Set Up date and time&nbsp;&nbsp;</label>
-        <input
-          type="datetime-local"
-          name="SetupDate"
-          onChange={(e) => handleChange(e)}
-          value={date.SetupDate}
-          id="setDate"
-        />
-      </div>
-      {locate==""&&<h3 style={{color:'red'}}>Select any</h3>}
       <div style={{ marginTop: "2%" }}>
-        <label htmlFor="location">Location&nbsp;&nbsp;</label>
-        <select id="location" value={locate} onChange={(e) => handleChanges(e)}>
-          {details.map((option) => (
-            <option key={option.Id} value={option.Id}>
-              {option.name}
-            </option>
-          ))}
-        </select>
+        {locate != "" ? (
+          <div
+            style={{ border: "1px solid black", backgroundColor: "whitesmoke" }}
+          >
+            <h2>Details of location</h2>
+            <span>
+              <h3>Duration:</h3>
+              {duration}
+            </span>
+            <span>
+              <h3>Venue:</h3> {details[locate].name}
+            </span>
+            <span>
+              <h3>Venue:</h3> {details[locate].name}
+            </span>
+            <span>
+              <h3>Transport Charge:</h3>{" "}
+              {Number(details[locate].Distance.split("k")[0]) * 2 * 50}
+            </span>
+            <span>
+              <h3>Distance:</h3> {details[locate].Distance}
+            </span>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
-      <button className="submita" onClick={submit}>
-        Enquiry
-      </button>
-      </div> 
-      <div style={{marginTop:"2%"}}>
-        {locate!=""?<div style={{border:"1px solid black",backgroundColor:"whitesmoke"}}>
-          <h2>Details of location</h2>
-          <span><h3>Venue:</h3> {details[locate].name}</span>
-          <span><h3>Transport Charge:</h3> {(Number(details[locate].Distance.split('k')[0])*2)*50}</span>
-          <span><h3>Distance:</h3> {details[locate].Distance}</span>
-        </div>:<></>}
-      </div>
-
     </div>
   );
 };
